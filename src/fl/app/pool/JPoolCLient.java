9730f1fa5b126f9a384449241e9f7ac13a9d0191
fl <fabouya@gmail.com>
2017-07-18 10:32:48 +0200

    boucle et acces serveur local

--- src/fl/app/pool/JPoolCLient.java
+++ src/fl/app/pool/JPoolCLient.java
@@ -2,9 +2,29 @@ package fl.app.pool;
 
 public class JPoolCLient {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
 
+		PoolCommand p = new PoolCommand("localhost", 5832);
+		if(p.PingServer())
+		{
+			try 
+			{
+				while(true)
+				{
+					String status = p.GetStatus();
+					System.out.println(status);
+					Thread.sleep(10000);
+				}
+			} 
+			catch (Exception e) 
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
 	}
 
 }
