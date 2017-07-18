db045ceca9b1e4b5ee6afcf6cc6b9632b4b41b33
fl <fabouya@gmail.com>
2017-07-18 15:26:09 +0200

    ajout command line (commons-cli-1.4)

--- src/fl/app/pool/JPoolCLient.java
+++ src/fl/app/pool/JPoolCLient.java
@@ -1,12 +1,79 @@
 package fl.app.pool;
 
-public class JPoolCLient {
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 
+public class JPoolCLient 
+{
+
+	private static String	_host = "";
+	private static String	_port = "";
+	private	static int		_delay = 1000;
+	
+	private static void DoParseOptions(String[] args)
+	{
+		// create Options object
+		Options options = new Options();
+
+
+		Option  host = Option.builder("h")
+						.hasArg()
+						.longOpt("host")
+						.required()
+						.argName("host")
+						.build();
+		
+		options.addOption(host);
+
+		Option  port = Option.builder("p")
+				.hasArg()
+				.longOpt("port")
+				.required()
+				.argName("port")
+				.build();
+
+		options.addOption(port);
+		
+		Option  delay = Option.builder("d")
+				.hasArg()
+				.longOpt("delay")
+				.required()
+				.argName("delay")
+				.build();
+
+		options.addOption(delay);
+		
+		
+		CommandLineParser parser = new DefaultParser();
+		
+		try 
+		{
+			CommandLine cmd = parser.parse( options, args);
+						
+			_host = cmd.getOptionValue("host");
+			_port = cmd.getOptionValue("port");
+			_delay = Integer.parseInt(cmd.getOptionValue("delay"));
+			
+		} catch (ParseException e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.exit(1);
+		}				
+		
+	}
+	
 	public static void main(String[] args) 
 	{
 		// TODO Auto-generated method stub
 
-		PoolCommand p = new PoolCommand("localhost", 5832);
+		DoParseOptions(args);
+		
+		PoolCommand p = new PoolCommand(_host, Integer.parseInt(_port));
 		if(p.PingServer())
 		{
 			try 
@@ -15,7 +82,7 @@ public class JPoolCLient {
 				{
 					String status = p.GetStatus();
 					System.out.println(status);
-					Thread.sleep(10000);
+					Thread.sleep(_delay);
 				}
 			} 
 			catch (Exception e) 
